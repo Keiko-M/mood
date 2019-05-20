5d0e062d210941300d33c8c32205c8677ad0db60
Keiko-M <morimoto.keiko@gmail.com>
2019-05-20 23:47:10 +0100

    tidy up function

--- models/moodslider.php
+++ models/moodslider.php
@@ -2,26 +2,27 @@
 
 //create ampty arrays (to be used to store each movie based on match level)
 $fourMatch = $threeMatch = $twoMatch = $oneMatch = $zeroMatch = array();
-function getData(){
+
+function processMatch($label1, $label2, $label3, $label4) {
     //Interprets an XML file into an object
     $xml = @simplexml_load_file("../uploads/Movies.xml") or die("Error: Cannot create object");
     //Converts to Json string - > Json Object for easier manupulation 
     $obj = json_decode(json_encode($xml));
     //Variable $programmes contains each programme in the xml file
     $programmes = $obj->programme;
-    return $programmes ; 
-}
-function processMatch($label1, $label2, $label3, $label4) {
+
     //for loop to fill the five arrays based on match level.
     for ($i = 0; $i < count($programmes); $i++) {
+
         //Create variable for label 1,2,3,4 of the movie data to be used as parameters below. 
         $movieLabel1 = $programmes[$i]->label1;
         $movieLabel2 = $programmes[$i]->label2;
         $movieLabel3 = $programmes[$i]->label3;
         $movieLabel4 = $programmes[$i]->label4;
-        
-        $mathlevel = checkMatch($movieLabel1, $movieLabel2, $movieLabel3, $movieLabel4, $label1, $label2, $label3, $label4);
-        arrayForMatchLevel($mathlevel);
+
+        //run function checkMatch and Create $matchResult to use the the match level. 
+        $matchResult = checkMatch($movieLabel1, $movieLabel2, $movieLabel3, $movieLabel4, $label1, $label2, $label3, $label4); 
+        createMatchArray($matchResult,$programmes[$i]);
     }
 }
 
@@ -43,7 +44,7 @@ function checkMatch($movieLabel1, $movieLabel2, $movieLabel3, $movieLabel4, $lab
     return $matchLevel;
 }
 
-function arrayForMatchLevel($matchResult) {
+function createMatchArray($matchResult,$programmes) {
     global $fourMatch;
     global $threeMatch;
     global $twoMatch;
@@ -51,15 +52,15 @@ function arrayForMatchLevel($matchResult) {
     global $zeroMatch;
     //Based on the match level, push the programm to the correct array 
     if ($matchResult == 4) {
-        array_push($fourMatch, $programmes[$i]);
+        array_push($fourMatch, $programmes);
     } elseif ($matchResult == 3) {
-        array_push($threeMatch, $programmes[$i]);
+        array_push($threeMatch, $programmes);
     } elseif ($matchResult == 2) {
-        array_push($twoMatch, $programmes[$i]);
+        array_push($twoMatch, $programmes);
     } elseif ($matchResult == 1) {
-        array_push($oneMatch, $programmes[$i]);
+        array_push($oneMatch, $programmes);
     } else {
-        array_push($zeroMatch, $programmes[$i]);
+        array_push($zeroMatch, $programmes);
     }
 }
 
@@ -76,14 +77,8 @@ function getMoviesByPreference() {
 function searchMovies($mood) {
     //Convert the json string from the GET to a json object
     $obj = json_decode($mood);
-    getData();
-    
     //Run processMatch based on user input
     processMatch($obj->label1, $obj->label2, $obj->label3, $obj->label4);
-     //run function checkMatch and Create $matchResult to use the the match level. 
-    $matchResult = checkMatch($movieLabel1, $movieLabel2, $movieLabel3, $movieLabel4, $label1, $label2, $label3, $label4);
-
-    
     // function getMoviesByPreference. $orderedResult will have all the programmes in order of match levels.
     $orderedResult = getMoviesByPreference();
 
